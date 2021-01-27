bb84bf9f6a73e309b30ccf3999d1340fc56c7786
MrBaduuz <joelwoerner@gmx.net>
2021-01-27 21:38:11 +0100

    Colorscheme

--- config.h
+++ config.h
@@ -1,27 +1,28 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Source Sans Pro:size=9.8", "NotoColorEmoji:fontformat=truetype:size=10:antialias=false", "Symbols Nerd Font:size=10" };
 static const char dmenufont[]       = "Source Sans Pro:size=9.8";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+
+
+static const char col_bg[]    = "#3c3836";
+static const char col_fg[]    = "#ebdbb2";
+static const char col_cyan[]  = "#427b58";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-	[SchemeHid]  = { col_cyan,  col_gray1, col_cyan  },
+	[SchemeNorm] = { col_fg,   col_bg,   col_bg   },
+	[SchemeSel]  = { col_fg,   col_cyan, col_cyan },
+	[SchemeHid]  = { col_cyan, col_bg,   col_cyan },
 };
 
 /* tagging */
