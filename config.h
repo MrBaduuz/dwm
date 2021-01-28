fe0524deee4d076e876b9bb67cbba12fc73854b6
MrBaduuz <joelwoerner@gmx.net>
2021-01-28 20:35:01 +0100

    Workspaces for applications & resize with kbd

--- config.h
+++ config.h
@@ -10,7 +10,7 @@ static const unsigned int gappov    = 10;       /* vert outer gap between window
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Source Sans Pro:size=9.8", "NotoColorEmoji:fontformat=truetype:size=10:antialias=false", "Symbols Nerd Font:size=10" };
+static const char *fonts[]          = { "Source Sans Pro:size=9.8", "NotoColorEmoji:fontformat=truetype:size=10:antialias=false", "Symbols Nerd Font:size=11" };
 static const char dmenufont[]       = "Source Sans Pro:size=9.8";
 
 
@@ -26,7 +26,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "阮", "4", "5", "漣", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -34,8 +34,10 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Gimp",       NULL,     NULL,       0,            1,           -1 },
+	{ "Pavucontrol",  NULL,   NULL,       1 << 5,       0,           -1 },
+	{ "Alacritty",  NULL,     "Spotify",       1 << 2,       0,           -1 },
+	{ "Alacritty",  NULL,     "Calendar",       1 << 3,       0,           -1 },
 };
 
 /* layout(s) */
@@ -78,7 +80,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-p", "Run:", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 static Key keys[] = {
@@ -125,7 +127,15 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	{ MODKEY,                       XK_s,      show,           {0} },
-	//{ MODKEY,                       XK_h,      hide,           {0} },
+	{ MODKEY,                       XK_w,      hide,           {0} },
+    { MODKEY|ShiftMask,			    XK_j,	   moveresize,		{.v = (int []){ 0, 25, 0, 0 }}},
+    { MODKEY|ShiftMask,			    XK_k,		moveresize,		{.v = (int []){ 0, -25, 0, 0 }}},
+    { MODKEY|ShiftMask,			    XK_l,	moveresize,		{.v = (int []){ 25, 0, 0, 0 }}},
+    { MODKEY|ShiftMask,			    XK_h,	moveresize,		{.v = (int []){ -25, 0, 0, 0 }}},
+    { MODKEY|ControlMask,		    XK_j,	moveresize,		{.v = (int []){ 0, 0, 0, 25 }}},
+    { MODKEY|ControlMask,		    XK_k,		moveresize,		{.v = (int []){ 0, 0, 0, -25 }}},
+    { MODKEY|ControlMask,		    XK_l,	moveresize,		{.v = (int []){ 0, 0, 25, 0 }}},
+    { MODKEY|ControlMask,		    XK_h,	moveresize,		{.v = (int []){ 0, 0, -25, 0 }}},
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
--- dwm.c
+++ dwm.c
@@ -199,6 +199,7 @@ static void mappingnotify(XEvent *e);
 static void maprequest(XEvent *e);
 static void monocle(Monitor *m);
 static void motionnotify(XEvent *e);
+static void moveresize(const Arg *arg);
 static void movemouse(const Arg *arg);
 static Client *nexttiled(Client *c);
 static void pop(Client *);
@@ -1327,6 +1328,21 @@ motionnotify(XEvent *e)
 		focus(NULL);
 	}
 	mon = m;
+}
+
+static void
+moveresize(const Arg *arg)
+{
+	XEvent ev;
+	Monitor *m = selmon;
+	if(!(m->sel && arg && arg->v && m->sel->isfloating))
+		return;
+	resize(m->sel, m->sel->x + ((int *)arg->v)[0],
+		m->sel->y + ((int *)arg->v)[1],
+		m->sel->w + ((int *)arg->v)[2],
+		m->sel->h + ((int *)arg->v)[3],
+		True);
+	while(XCheckMaskEvent(dpy, EnterWindowMask, &ev));
 }
 
 void
