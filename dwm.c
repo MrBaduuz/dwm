e7ad681f3a3a1cacbb370c299c9f7a1ea61ad964
MrBaduuz <joelwoerner@gmx.de>
2021-02-14 21:56:33 +0100

    Added status on all monitors

--- dwm.c
+++ dwm.c
@@ -871,7 +871,7 @@ drawbar(Monitor *m)
 		stw = getsystraywidth();
 
 	/* draw status first so it can be overdrawn by tags later */
-	if (m == selmon) { /* status is only drawn on selected monitor */
+	if (m == selmon || 1) { /* status is only drawn on selected monitor */
 		drw_setscheme(drw, scheme[SchemeNorm]);
 		tw = TEXTW(stext) - lrpad / 2 + 2; /* 2px right padding */
 		drw_text(drw, m->ww - tw - stw, 0, tw, bh, lrpad / 2 - 2, stext, 0);
@@ -2487,7 +2487,8 @@ updatestatus(void)
 		strcpy(stext, "dwm-"VERSION);
 	else
 		copyvalidchars(stext, rawstext);
-	drawbar(selmon);
+	for(Monitor* m = mons; m; m = m->next)
+		drawbar(m);
 	updatesystray();
 }
 
